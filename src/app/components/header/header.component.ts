4d71e524ad3cb205604eff5f423b7e9e44e84c0c
pamelanancupil <pamela.nancupilv@gmail.com>
2020-09-21 03:12:53 -0300

    rutas header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -12,4 +12,17 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  toHome(){
+    document.getElementById('home').scrollIntoView({behavior:"smooth"})
+  }
+  toFunctioning(){
+    document.getElementById('functioning').scrollIntoView({behavior:"smooth"})
+  }
+  toServicios(){
+    document.getElementById('servicios').scrollIntoView({behavior:"smooth"})
+  }
+  toContact(){
+    document.getElementById('contact').scrollIntoView({behavior:"smooth"})
+  }
+
 }
